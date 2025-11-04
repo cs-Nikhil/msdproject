268d784dbf73b19deb410915f8da0bd58144e8ce
cs-Nikhil <231fa04c88@gmail.com>
2025-11-04 19:21:26 +0530

    Update Navbar.jsx

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -16,7 +16,7 @@ const Navbar = () => {
       <div className="container mx-auto px-4">
         <div className="flex justify-between items-center py-4">
           <div className="flex items-center space-x-4">
-            <Link to="/" className="text-xl font-bold text-blue-600">
+            <Link to="#" className="text-xl font-bold text-blue-600">
               Hospital Management
             </Link>
           </div>
